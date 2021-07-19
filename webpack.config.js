ad32e7dde633d84ec0af4b7e2a2a1351264683e3
Nestor Fabio Gomez Toro <neyblue15@hotmail.com>
2021-07-19 13:22:05 -0500

    cambio a https

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/[name].[hash].js",
-    publicPath: "http://Nestor2018.github.io/CLOTHESSTORE", // cdn donde se leera el proyecto
+    publicPath: "https://Nestor2018.github.io/CLOTHESSTORE", // cdn donde se leera el proyecto
     chunkFilename: "js/[id].[chunkhash].js",
   },
   optimization: {
@@ -76,7 +76,7 @@ module.exports = {
     new AddAssetHtmlPlugin({
       filepath: path.resolve(__dirname, "dist/js/*.dll.js"),
       outputPath: "js",
-      publicPath: "http://Nestor2018.github.io/CLOTHESSTORE",
+      publicPath: "https://Nestor2018.github.io/CLOTHESSTORE",
     }),
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: ["**/app.*"], // borra solo los archivos que tengan app.js o css par auqe no borre las imagenes
